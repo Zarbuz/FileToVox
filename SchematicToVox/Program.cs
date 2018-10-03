f809515bbacd4b45ddc836a60be37fd125f94164
Zarbuz <nicolas.perrier2@numericable.fr>
2018-10-03 18:08:55 +0200

    Moved global position of regions to be in the center in MV

--- SchematicToVox/Program.cs
+++ SchematicToVox/Program.cs
@@ -1,6 +1,7 @@
 ﻿using SchematicToVox.Vox;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,14 +12,17 @@ namespace SchematicToVox
     {
         static void Main(string[] args)
         {
+            var path = "../../schematics/gateway.schematic";
+            var schematic = SchematicReader.SchematicReader.LoadSchematic(path);
+            var name = Path.GetFileNameWithoutExtension(path);
+
+            VoxWriter writer = new VoxWriter();
+            writer.WriteModel("../../exports/" + name + ".vox", schematic);
+
             //VoxReader reader = new VoxReader();
             //VoxModel voxModel = new VoxModel();
-            //reader.LoadModel("../../exports/export.vox", voxModel);
-
-            var schematic = SchematicReader.SchematicReader.LoadSchematic("../../schematics/5.schematic");
-            VoxWriter writer = new VoxWriter();
-            writer.WriteModel("../../exports/" + "export" + ".vox", schematic);
-
+            //reader.LoadModel("../../exports/gateway.vox", voxModel);
+            Console.WriteLine("Done");
             Console.ReadLine();
         }
 
