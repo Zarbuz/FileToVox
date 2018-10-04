f37612634b555f6403fda261bd7298e515cf227a
Zarbuz <nicolas.perrier2@numericable.fr>
2018-10-04 16:55:03 +0200

    Added custom extensions for this project

---
+++ SchematicToVox/Extensions/Extensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SchematicToVox.Extensions
+{
+    public static class Extensions
+    {
+        public static HashSet<T> ToHashSet<T>(
+            this IEnumerable<T> source,
+            IEqualityComparer<T> comparer = null)
+        {
+            return new HashSet<T>(source, comparer);
+        }
+    }
+}
