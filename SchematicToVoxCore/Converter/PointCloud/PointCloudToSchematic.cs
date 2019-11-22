d5c038a85290539599d6f2ab5176de589d4a0c3b
Nicolas Perrier <nperrier@manzalab.com>
2019-11-22 22:07:32 +0100

    Updated PointCloud

--- SchematicToVoxCore/Converter/PointCloud/PointCloudToSchematic.cs
+++ SchematicToVoxCore/Converter/PointCloud/PointCloudToSchematic.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Linq;
 using FileToVox.Schematics;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using FileToVox.Utils;
 
 namespace FileToVox.Converter.PointCloud
@@ -27,20 +29,21 @@ namespace FileToVox.Converter.PointCloud
                         for (ushort x = 0; x < schematic.Width; x++)
                         {
                             hashSet.TryGetValue(new Block(x, y, z, 0), out Block block);
-                            if (block.IsDefaultValue())
+                            if (block.IsDefaultValue() && x > 0 && x < schematic.Width && z > 0 && z < schematic.Length)
                             {
-                                if (x > 0 && x < schematic.Width && z > 0 && z < schematic.Length)
-                                {
-                                    hashSet.TryGetValue(new Block((ushort) (x - 1), y, z, 0), out Block left);
-                                    hashSet.TryGetValue(new Block((ushort) (x + 1), y, z, 0), out Block right);
-                                    hashSet.TryGetValue(new Block(x, y, (ushort) (z - 1), 0), out Block top);
-                                    hashSet.TryGetValue(new Block(x, y, (ushort) (z + 1), 0), out Block bottom);
+                                hashSet.TryGetValue(new Block((ushort)(x - 1), y, z, 0), out Block left);
+                                hashSet.TryGetValue(new Block((ushort)(x + 1), y, z, 0), out Block right);
+                                hashSet.TryGetValue(new Block(x, y, (ushort)(z - 1), 0), out Block top);
+                                hashSet.TryGetValue(new Block(x, y, (ushort)(z + 1), 0), out Block bottom);
 
-                                    if (!left.IsDefaultValue() && !right.IsDefaultValue() && !top.IsDefaultValue() &&
-                                        !bottom.IsDefaultValue())
-                                    {
-                                        hashSet.Add(new Block(x, y, z, left.Color));
-                                    }
+                                hashSet.TryGetValue(new Block((ushort)(x+1), y, (ushort)(z+1), 0), out Block diagonal);
+
+                                List<Block> blocks = new List<Block>();
+                                blocks.AddRange(new[] { left, right, top, bottom });
+                                if (blocks.Count(t => !t.IsDefaultValue()) >= 3)
+                                {
+                                    hashSet.Add(new Block(x, y, z, blocks.First(t => !t.IsDefaultValue()).Color));
+                                    x = 0;
                                 }
                             }
                         }
@@ -50,5 +53,6 @@ namespace FileToVox.Converter.PointCloud
             }
             Console.WriteLine("[LOG] Done.");
         }
+
     }
 }
