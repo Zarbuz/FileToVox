96cc10c1ac5777cf7058536677d132241a859e88
Nicolas Perrier <nperrier@manzalab.com>
2022-09-19 14:43:47 +0200

    Removed folder Quantizer

--- SchematicToVoxCore/Extensions/Quantization.cs
+++ SchematicToVoxCore/Extensions/Quantization.cs
@@ -3,7 +3,6 @@ using FileToVoxCore.Utils;
 using ImageMagick;
 using System;
 using System.Collections.Generic;
-using System.Drawing;
 using System.Linq;
 using Color = FileToVoxCore.Drawing.Color;
 
