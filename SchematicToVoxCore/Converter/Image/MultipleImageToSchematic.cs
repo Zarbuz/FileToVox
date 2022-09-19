f6011506fc27277b4825cb69e35406e2b31dd70a
Nicolas Perrier <nperrier@manzalab.com>
2022-09-19 14:50:38 +0200

    Fixed parsing pixel color in MultipleImageToSchematic

--- SchematicToVoxCore/Converter/Image/MultipleImageToSchematic.cs
+++ SchematicToVoxCore/Converter/Image/MultipleImageToSchematic.cs
@@ -28,14 +28,18 @@ namespace FileToVox.Converter.Image
 			Console.WriteLine("[INFO] Total images to process: " + mImages.Count);
 
 			List<Voxel> blocks = new List<Voxel>();
-			Bitmap bitmapColor = null;
+			IPixelCollection<ushort> pixelsColor = null;
+			int colorWidth = 0;
 			if (mInputColorFile != null)
 			{
-				bitmapColor = new Bitmap(mInputColorFile);
+				MagickImage bitmapColor = new MagickImage(mInputColorFile);
 				if (bitmapColor.Width > 256 || bitmapColor.Height > 1)
 				{
 					throw new ArgumentException("[ERROR] The input color file must have a dimension of 256x1 px");
 				}
+
+				colorWidth = bitmapColor.Width;
+				pixelsColor = bitmapColor.GetPixels();
 			}
 
 			for (int i = 0; i < mImages.Count; i++)
@@ -43,7 +47,7 @@ namespace FileToVox.Converter.Image
 				string file = mImages[i];
 				Console.WriteLine("[INFO] Reading file: " + file);
 				MagickImage bitmap = new MagickImage(file);
-				var pixels = bitmap.GetPixels();
+				IPixelCollection<ushort> pixels = bitmap.GetPixels();
 
 				for (int x = 0; x < bitmap.Width; x++)
 				{
@@ -59,7 +63,7 @@ namespace FileToVox.Converter.Image
 								double distance = Math.Sqrt(Math.Pow((height / 2) - x, 2) + Math.Pow((height / 2) - y, 2));
 								float range = (float)Math.Abs(distance / (height / 2)); //
 								range = range > 1 ? 1 : range;
-								color = bitmapColor.GetPixel((int)(range * (bitmapColor.Width - 1)), 0);
+								color = pixelsColor.GetPixel((int)(range * (colorWidth - 1)), 0).GetPixelColor();
 							}
 
 							if (mExcavate)
@@ -88,17 +92,13 @@ namespace FileToVox.Converter.Image
 
 			if (x - 1 >= 0 && x + 1 < bitmap.Width && y - 1 >= 0 && y + 1 < bitmap.Height)
 			{
-				IPixel<ushort> pixelLeft = pixels.GetPixel(x - 1, y);
-				Color left = Color.FromArgb(pixelLeft.GetChannel(4), pixelLeft.GetChannel(0), pixelLeft.GetChannel(1), pixelLeft.GetChannel(2));
+				Color left = pixels.GetPixel(x - 1, y).GetPixelColor();
 
-				IPixel<ushort> pixelTop = pixels.GetPixel(x , y -1);
-				Color top = Color.FromArgb(pixelTop.GetChannel(4), pixelTop.GetChannel(0), pixelTop.GetChannel(1), pixelTop.GetChannel(2));
+				Color top = pixels.GetPixel(x, y - 1).GetPixelColor();
 
-				IPixel<ushort> pixelRight = pixels.GetPixel(x + 1, y);
-				Color right = Color.FromArgb(pixelRight.GetChannel(4), pixelRight.GetChannel(0), pixelRight.GetChannel(1), pixelRight.GetChannel(2));
+				Color right = pixels.GetPixel(x + 1, y).GetPixelColor();
 
-				IPixel<ushort> pixelBottom = pixels.GetPixel(x, y + 1);
-				Color bottom = Color.FromArgb(pixelBottom.GetChannel(4), pixelBottom.GetChannel(0), pixelBottom.GetChannel(1), pixelBottom.GetChannel(2));
+				Color bottom = pixels.GetPixel(x, y + 1).GetPixelColor();
 
 				bool leftColor = left != Color.Empty && left != Color.Transparent && left != Color.Black && (left.R != 0 && left.G != 0 && left.B != 0);
 				bool topColor = top != Color.Empty && top != Color.Transparent && top != Color.Black && (top.R != 0 && top.G != 0 && top.B != 0);
